a26cc34beec44e9325f3f3870e0acbfd170a0805
Lin <Lin>
2018-04-22 17:33:19 -0400

    Movie clip table created

--- DatabaseApplicationV2/app/Movie.php
+++ DatabaseApplicationV2/app/Movie.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Movie_poster;
+use App\Movie_clip;
+
 class Movie extends Model
 {
     public function movie_poster(){
@@ -11,5 +13,12 @@ class Movie extends Model
     }
     public function movie_posters(){
       return $this->hasMany(Movie_poster::class);
+    }
+    
+    public function movie_clip(){
+      return $this->hasOne(Movie_clip::class);
+    }
+    public function movie_clips(){
+      return $this->hasMany(Movie_clip::class);
     }
 }
