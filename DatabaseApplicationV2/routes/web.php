d41476f5db7a7ecb4d673ec6a1b5cd48ef6e48ff
Linyi <jianglinyi97@gmail.com>
2018-04-25 16:35:08 -0400

    Revert "Revert "Like and unlike work.""
    This reverts commit 3da457e9857f46bc514e94605d5c6be85e781881.

--- DatabaseApplicationV2/routes/web.php
+++ DatabaseApplicationV2/routes/web.php
@@ -71,3 +71,4 @@ Route::post('/movies/clip/{id}','MovieClipController@delete');
 Route::get('/movies/setTrailer/{id}','MovieClipController@setToTrailer');
 
 Route::post('/like','UserLikeController@like');
+Route::post('/unlike','UserLikeController@unlike');
