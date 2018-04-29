dcf9ee13e2c20c94c3a8fa267638e0b14d3e41e3
Mark-Ellis Cuenca <mark1408@live.ca>
2018-04-29 14:36:03 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/test-master-branch' into evan's-current-wrking-real-branch""
    This reverts commit 0d31b10c0c1cface1e1068172f27d2f769124223.

--- DatabaseApplicationV2/resources/views/support.blade.php
+++ DatabaseApplicationV2/resources/views/support.blade.php
@@ -4,15 +4,7 @@
 <h1>Support</h1>
 <p>Got any issues? Report them here and our adminstrators can help you!</p>
 <hr>
-  {!! Form::open(['url' => 'contact/submit']) !!}
-  <div class="form-group">
-    {{Form::label('name', 'Name')}}
-    {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Enter Name'])}}
-  </div>
-  <div class="form-group">
-    {{Form::label('email', 'E-Mail Address')}}
-    {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Enter email'])}}
-  </div>
+  {!! Form::open(['url' => 'support/submit']) !!}
   <div class="form-group">
     {{Form::label('message', 'Message')}}
     {{Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'Enter Message'])}}
---
+++ DatabaseApplicationV2/resources/views/support/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('content')
+<h1>View Support Ticket Details</h1>
+<hr/>
+<div class="jumbotron">
+  <p><b>Ticket Submitted by:</b> {{\App\Http\Controllers\UserController::getUsername($ticket->user_id)}}</p>
+  <p><b>User ID: </b>{{$ticket->user_id}}</p>
+  <p><b>Ticket Submit Date: </b>
+     {{$ticket->created_at}}
+   </p>
+  <p><b>Message: </b></p>
+  <textarea readonly rows="15" cols="70" style="resize: none">
+    {{$ticket->message}}
+  </textarea>
+  <p><b>Status: </b>{{$ticket->status}}</p>
+</div>
+<div class="jumbotron">
+  <a href="/support/index" class="btn btn-primary">< Go Back To Ticket List</a>
+</div>
+@endsection
