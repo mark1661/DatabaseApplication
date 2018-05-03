3e596271adf383f66c236d2db4f7196f7b245999
Linyi <jianglinyi97@gmail.com>
2018-05-03 15:11:08 -0400

    Showing movie clip fixed

--- DatabaseApplicationV2/resources/views/Movies/detail.blade.php
+++ DatabaseApplicationV2/resources/views/Movies/detail.blade.php
@@ -44,11 +44,47 @@ $(document).on('click','#unlike', function(){
 </script>
 <br/>
 <div class="jumbotron">
+  @if(Auth::check()==true)
+  <!-- Add to my list -->
+  <?php
+    $found=false;
+    if (Session::has('item')) {
+      $items = Session::get('item');
+      foreach ($items as $item) {
+        if ($item->id==$movie->id) {
+          $found=true;
+        }
+      }
+      if ($found==true) {
+        echo "<a class='btn btn-warning disabled'>Movie is already in your list!</a>";
+      }
+      else{
+        echo "<a class='btn btn-primary' href=\"/list/add/$movie->id\">Add this movie to my list!</a>";
+      }
+    }
+    else {
+      echo "<a class='btn btn-primary' href=\"/list/add/$movie->id\">Add this movie to my list!</a>";
+    }
+  ?>
+
+  @if($movie->likes->isEmpty())
+    <button id="like" class="btn btn-success">Like {{count($movie->likes)}}</button>
+  @else
+    @foreach($movie->likes as $like)
+      @if(($like->user_id)!=Auth::user()->user_id)
+        <button id="like" class="btn btn-success">Like {{count($movie->likes)}}</button>
+      @break
+      @else
+        <button id="unlike" class="btn btn-danger">Unlike</button>
+      @endif
+    @endforeach
+   @endif
+  @endif
   <table class="table">
     <tbody>
       <tr>
         <td style="background-color: navy; color: white">Movie poster: </td>
-        <td style="background-color: white" id="movieposter">{{$movie->poster}}</td>
+        <td style="background-color: white" id="movieposter"><img class="card-img-top" width="200" height="300" src="{{ Storage::url($movie->movie_poster->path)}}" alt="Card image cap"></td>
       </tr>
       <tr>
         <td style="background-color: navy; color: white">Movie Plot: </td>
@@ -61,27 +97,45 @@ $(document).on('click','#unlike', function(){
       <tr>
         <td style="background-color: navy; color: white">Movie Genre: </td>
         <td style="background-color: white" id="moviegenre">{{$movie->genre}}</td>
+      </tr>
+      <tr>
+        <td style="background-color: navy; color: white">Movie Trailer: </td>
+        <td style="background-color: white" id="moviereleasedate">
+            @if(isset($movie->movie_clip))
+              <video width="320" height="240" controls>
+              <source src="{{Storage::url($movie->movie_clip->path)}}" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            @endif
+        </td>
+      </tr>
         @if(Auth::check()==true)
           @if(Auth::user()->status == 'ADMIN')
             <tr>
-              <td style="background-color: navy; color: white">Movie Clips: </td>
+              <td style="background-color: navy; color: white">Manage Movie Clips: </td>
               <td style="background-color: white" id="Movie_clip">
                 <ul>
                   @foreach($movie->movie_clips as $movie_clip)
-                    <li>{{ $movie_clip->file_name }}
-                    Belongs to: {{ $movie_clip->user->username }}
-                    <a href="/movies/setTrailer/{{$movie_clip->id}}">Set it to trailer</a>
-                    <a href="/movies/clip/{{$movie_clip->id}}">Delete</a>
-                  </li>
+                    <li>
+                      Belongs to: {{ $movie_clip->user->username }}
+                      <br>
+                      <video width="320" height="240" controls>
+                        <source src="{{Storage::url($movie->movie_clip->path)}}" type="video/mp4">
+                        Your browser does not support the video tag.
+                      </video>
+                      <br>
+                      <a href="/movies/setTrailer/{{$movie_clip->id}}">Set it to trailer</a> |
+                      <a href="/movies/clip/{{$movie_clip->id}}">Delete</a>
+                    </li>
                   @endforeach
                 </ul>
               </td>
             </tr>
           @endif
         @endif
-      </tr>
     </tbody>
   </table>
+
   @isset($reviews)
     @foreach($reviews as $review)
       <div class="form-group">
@@ -96,48 +150,11 @@ $(document).on('click','#unlike', function(){
       <label for="upload">Upload movie clips:</label>
       <input type="file" class="form-control-file" name="clip" id="upload">
     </div>
-    @if(Auth::check()==true)
-      <!-- Add to my list -->
-      <?php
-        $found=false;
-        if (Session::has('item')) {
-          $items = Session::get('item');
-          foreach ($items as $item) {
-            if ($item->id==$movie->id) {
-              $found=true;
-            }
-          }
-          if ($found==true) {
-            echo "<a class='btn btn-warning disabled'>Movie is already in your list!</a>";
-          }
-          else{
-            echo "<a class='btn btn-primary' href=\"/list/add/$movie->id\">Add this movie to my list!</a>";
-          }
-        }
-        else {
-          echo "<a class='btn btn-primary' href=\"/list/add/$movie->id\">Add this movie to my list!</a>";
-        }
-       ?>
-
-      @if($movie->likes->isEmpty())
-        <button id="like" class="btn btn-success">Like {{count($movie->likes)}}</button>
-      @else
-        @foreach($movie->likes as $like)
-          @if(($like->user_id)!=Auth::user()->user_id)
-            <button id="like" class="btn btn-success">Like {{count($movie->likes)}}</button>
-            @break
-          @else
-            <button id="unlike" class="btn btn-danger">Unlike</button>
-          @endif
-        @endforeach
-      @endif
-    @endif
-    <br>
-    <br>
-    <a class="btn btn-info" href="/createReview/{{$movie->id}}">Add a new review!</a>
+    <button type="submit" class="btn btn-primary">Submit</button>
   </form>
+  <br>
+    <a class="btn btn-info" href="/createReview/{{$movie->id}}">Add a new review!</a>
   <hr/>
   <a class="btn btn-primary" href="/movies">< Back</a>
-  <button type="submit" class="btn btn-primary">Submit</button>
 </div>
 @endsection
